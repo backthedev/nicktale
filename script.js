921c6ad2fbd970795777ddefeef268da0878c4f3
backthedev <160367766+backthedev@users.noreply.github.com>
2024-02-17 19:43:00 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function copiarTexto() {
+    var texto = document.getElementById("textoACopiar");
+    var input = document.createElement('input');
+    input.setAttribute('value', texto.textContent);
+    document.body.appendChild(input);
+    input.select();
+    document.execCommand('copy');
+    document.body.removeChild(input);
+    alert("¡IP copiada!");
+  }
